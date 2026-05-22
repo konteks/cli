195b23eeb74a996b16e349cfcdb9824cfe10f63b
🍕 <zainadam.id@gmail.com>
2026-05-22 17:53:14 +0800

    simplify tool debug command

--- src/entrypoints/cli/commands/mcp/tools-command.ts
+++ src/entrypoints/cli/commands/mcp/tools-command.ts
@@ -1,13 +1,62 @@
-import mcpTools from '@/entrypoints/mcp/tools'
-import printJson from '@/support/cli/print-json'
-import type { BaseCommandInput } from '../_base-command'
+import { confirm, select } from '@inquirer/prompts'
+import MCP_TOOLS from '@/entrypoints/mcp/tools'
 import BaseCommand from '../_base-command'
 
 export default class ToolsCommand extends BaseCommand {
     public readonly description = 'List MCP tools exposed by Konteks.'
     public readonly name = 'tools'
 
-    public async handle(_input: BaseCommandInput): Promise<void> {
-        printJson(mcpTools)
+    public async handle(): Promise<void> {
+        let loop = true
+
+        while (loop) {
+            const selectedToolName = await selectTool()
+
+            const selectedTool = MCP_TOOLS.find(
+                tool => tool.name === selectedToolName,
+            )
+
+            if (!selectedTool) {
+                throw new Error(`Unknown tool: ${selectedToolName}`)
+            }
+
+            printTool(selectedTool)
+            const isConfirmed = await callToolConfirmation()
+
+            if (isConfirmed) {
+                await selectedTool.handle({})
+
+                loop = await confirm({
+                    default: true,
+                    message: 'Call another tool?',
+                })
+            }
+        }
     }
+}
+
+function selectTool() {
+    return select({
+        choices: MCP_TOOLS.map(tool => ({
+            name: `${tool.name} - ${tool.description}`,
+            value: tool.name,
+        })),
+        message: 'Select a tool:',
+    })
+}
+
+function printTool(tool: (typeof MCP_TOOLS)[number]) {
+    console.log({
+        annotations: tool.annotations,
+        description: tool.description,
+        inputSchema: 'COMING SOON',
+        name: tool.name,
+    })
+}
+
+function callToolConfirmation() {
+    return confirm({
+        default: true,
+        message: 'Call this tool?',
+    })
 }
